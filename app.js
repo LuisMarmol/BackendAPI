0bbfbdeab2431528af84eef9c9811783dc0616a1
LuisMarmol <luis.marmol@tecnologicoliceocristiano.edu.ec>
2024-05-14 20:00:19 -0500

    Added Validation Route

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes);
+
+app.use((req, res, next) => {
+    const error = new HttpError('Ruta no existente', 404);
+    throw error;
+});
 
 //manejo de errores
 app.use((error, req, res, next)=>{
